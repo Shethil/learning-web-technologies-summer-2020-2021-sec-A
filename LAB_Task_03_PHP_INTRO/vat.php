d2d1db12ede1cfc17bbfe589cc6cef0aea0db48f
Shethil Ahammed <shithelahammed@gmail.com>
2021-06-13 10:56:53 +0600

    vat php

---
+++ LAB_Task_03_PHP_INTRO/vat.php
@@ -0,0 +1,11 @@
+<?php
+
+// Write a PHP script to calculate the VAT (Value Added Tax) over an amount. Hints: VAT = 15% of the amount
+
+$amount=100;
+
+$VAT= ($amount*15)/100;
+
+echo "The VAT is : $VAT";
+
+?>
