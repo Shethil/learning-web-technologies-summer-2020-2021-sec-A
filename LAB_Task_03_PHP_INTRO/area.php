110466e5538a332c6ddc8164d3ca6c88c4f4bc33
Shethil Ahammed <shithelahammed@gmail.com>
2021-06-13 10:55:17 +0600

    area php

---
+++ LAB_Task_03_PHP_INTRO/area.php
@@ -0,0 +1,10 @@
+<?php  
+
+//The area of a Rectangle = length × width, perimeter = 2 × (length + width)
+
+ $length = 20;  
+ $width = 12;  
+
+ echo "area of rectangle is $length * $width= " . ($length * $width) . "<br />";  
+
+?>
