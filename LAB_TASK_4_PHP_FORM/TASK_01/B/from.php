0cc467bab1a7f6ef3cefaca5e54cc2aa8a8d49c8
Shethil Ahammed <shithelahammed@gmail.com>
2021-06-15 18:49:36 +0600

    Lab 04 Task 01 B

---
+++ LAB_TASK_4_PHP_FORM/TASK_01/B/from.php
@@ -0,0 +1,32 @@
+<?php
+	
+	$username = "";
+	
+	if(isset($_REQUEST['submit'])){
+		
+		$username = $_REQUEST['username'];
+		if($username == ""){
+			echo "invalid username!";
+		}
+		else{
+			echo $username;
+		}
+	}else{
+		//echo 'invalid request';
+
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Data</title>
+</head>
+<body>
+	<form method="post" >
+
+		Username: <input type="text" name="username" value="">
+				<input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
